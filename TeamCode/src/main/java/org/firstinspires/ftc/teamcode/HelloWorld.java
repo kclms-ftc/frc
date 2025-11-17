f01d349b3b9dd5b6f689f9c8b07db859a4ad1782
Kishika <kishi.j21@gmail.com>
2025-11-17 02:41:33 +0000

    Create HelloWorld.java
    created first java glass

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+@Autonomous
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello", "Kishi");
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+    // Single line comment
+
+    /*
+    Block comment
+     */
+}
