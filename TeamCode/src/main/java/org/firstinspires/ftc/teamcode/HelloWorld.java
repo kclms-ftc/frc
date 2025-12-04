947cd41bf8faf135ed48b44b218c3dd188f7215c
Moyinoluwa Akinnawonu <moyinaki@gmail.com>
2025-12-04 18:35:16 +0000

    halfway thro first file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp // need this for the code to show up in driver station
+// class also needs to be public for the same reason
+// helloworld = child of opmode
+public class HelloWorld extends OpMode {
+
+    @Override // means the opmode class were extending from has an init method,
+    public void init(){ // we are overriding this init method
+        telemetry.addData("Hello", "World"); // all statments end in ; or {
+
+    }
+    public void loop(){
+
+    }
+}
