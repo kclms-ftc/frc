5ac25b71e233a7b74890ccb94a38d099d2ef3444
Moyinoluwa Akinnawonu <moyinaki@gmail.com>
2025-12-08 19:29:13 +0000

    first video

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld2.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+// Opmodes -> e.g. teleop / autonomous
+// all opmodes need 2 methods -> init + loop
+
+@TeleOp // need @teleop or @autonomous for all driver station code
+public class HelloWorld2 extends OpMode{
+
+    @Override // overrides og codes opmode
+    public void init() { // void = not doing anything
+        telemetry.addData("Hello", "World"); // all end in ; or {
+    }
+
+    @Override
+    public void loop() {
+        
+    }
+}
