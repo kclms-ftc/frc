67eec1658c9881c9ac14e491f3a3c5c3dd2bd076
Kavan Vyas <kavanvy@gmail.com>
2026-02-08 16:57:11 +0000

    Update HardwareMapConfig.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMapConfig.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HardwareMapConfig.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
@@ -8,30 +7,35 @@ import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
 public class HardwareMapConfig {
 
-    public DcMotorEx wheel_0, wheel_1, wheel_2, wheel_3;
+    public DcMotorEx turret_motor;
 
-    public DcMotorEx shooter_motor_0, shooter_motor_1;
     public DcMotorEx intake_motor;
 
+    public DcMotorEx shooter_motor_0, shooter_motor_1;
+
+    public DcMotorEx wheel_0, wheel_1, wheel_2, wheel_3;
+
+    public Servo feeder_servo;
     public Servo angle_servo;
-    // public Servo servo_1;
 
     public WebcamName webcam;
 
     public HardwareMapConfig(HardwareMap hw) {
+
+        turret_motor = hw.get(DcMotorEx.class, "turret_motor");
+
+        intake_motor = hw.get(DcMotorEx.class, "intake_motor");
+
+        shooter_motor_0 = hw.get(DcMotorEx.class, "shooter_motor_0");
+        shooter_motor_1 = hw.get(DcMotorEx.class, "shooter_motor_1");
 
         wheel_0 = hw.get(DcMotorEx.class, "wheel_0");
         wheel_1 = hw.get(DcMotorEx.class, "wheel_1");
         wheel_2 = hw.get(DcMotorEx.class, "wheel_2");
         wheel_3 = hw.get(DcMotorEx.class, "wheel_3");
 
-        shooter_motor_0 = hw.get(DcMotorEx.class, "shooter_motor_0");
-        shooter_motor_1 = hw.get(DcMotorEx.class, "shooter_motor_1");
-
-        intake_motor = hw.get(DcMotorEx.class, "intake_motor");
-
+        feeder_servo = hw.get(Servo.class, "feeder_servo");
         angle_servo = hw.get(Servo.class, "angle_servo");
-        // servo_1 = hw.get(Servo.class, "servo_1");
 
         webcam = hw.get(WebcamName.class, "webcam");
     }
