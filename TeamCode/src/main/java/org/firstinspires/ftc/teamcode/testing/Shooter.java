4595681bf076c66fc76222f260a5a9895a18c12a
Gergana <gdecheva25@gmail.com>
2026-02-13 15:30:28 +0000

    Update Shooter.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Shooter.java
@@ -29,6 +29,9 @@ public class Shooter {
     double REST_POS = 0.0;
     double PUSH_POS = 0.5; // Adjust until the arm hits the ball correctly
     double targetRPM = 1500;
+    double lowTargetRPM = 900;
+    double highTargetRPM = 1500;
+    double curTargetRPM = 0;
 
     // Telemetry storage variables
     double actualVelocity0 = 0;
@@ -40,10 +43,12 @@ public class Shooter {
         this.shooter_0 = config.shooter_motor_0;
         this.shooter_1 = config.shooter_motor_1;
         this.feeder = config.feeder_servo;
+
         shooter_0.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         shooter_1.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
         shooter_0.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
         shooter_1.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
         PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P,0,0,F);
         shooter_0.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidfCoefficients);
         shooter_1.setPIDFCoefficients(DcMotorEx.RunMode.RUN_USING_ENCODER, pidfCoefficients);
@@ -54,10 +59,10 @@ public class Shooter {
         this.targetRPM = target;
         double triggerPressure = gamepad.left_trigger;
 
-        if (triggerPressure > 0.1){
+        if (triggerPressure > 0.1) {
             shooter_0.setVelocity(targetRPM);
             shooter_1.setVelocity(targetRPM);
-        }else {
+        } else {
             shooter_0.setVelocity(0);
             shooter_1.setVelocity(0);
         }
@@ -66,6 +71,45 @@ public class Shooter {
         double actualVelocity1 = shooter_1.getVelocity();
         double velocityError0 = targetRPM - actualVelocity0;
         double velocityError1 = targetRPM - actualVelocity1;
+
+        // Brogan code
+        if (gamepad.yWasPressed()) { // toggles target speed
+            if (targetRPM == highTargetRPM) {
+                targetRPM = lowTargetRPM;
+            } else {
+                targetRPM = highTargetRPM;
+            }
+
+            if (gamepad.bWasPressed()) {
+                stepIndex = (stepIndex + 1) % stepSizes.length; // cycles through stepSizes list
+            }
+        }
+    }
+
+    public void changeRPM(Gamepad gamepad) {
+        if (gamepad.dpadLeftWasPressed()){
+            F += stepSizes[stepIndex];
+        }
+        if (gamepad.dpadRightWasPressed()){
+            F -= stepSizes[stepIndex];
+        }
+        if (gamepad.dpadUpWasPressed()){
+            P += stepSizes[stepIndex];
+        }
+        if (gamepad.dpadDownWasPressed()){
+            P -= stepSizes[stepIndex];
+        }
+
+        // set new pidf coefficients
+
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P,0,0,F);
+        shooter_0.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        shooter_1.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+
+        double curVelocity0 = shooter_0.getVelocity();
+        double curVelocity1 = shooter_1.getVelocity();
+
+
     }
 
     public void stopFlyWheel() {
@@ -99,7 +143,10 @@ public class Shooter {
         telemetry.addData("Motor State: ", isSpinning ? "SPINNING" : "STOPPED");
         telemetry.addData("Actual Velocity 0: ", "%.2f ticks/s", actualVelocity0);
         telemetry.addData("Actual Velocity 1: ", "%.2f ticks/s", actualVelocity1);
-
+        telemetry.addLine("-----------------------------");
+        telemetry.addData("Tuning P", "%.4f (D-Pad U/D",P);
+        telemetry.addData("Tuning F", "%.4f (D-Pad L/R",  F);
+        telemetry.addData("Step Size", "%.4f (B Button)", stepSizes[stepIndex]);
         telemetry.addLine("--- SERVO ARM STATUS ---");
         telemetry.addData("Servo Pos: ", feeder.getPosition());
 
@@ -114,6 +161,7 @@ public class Shooter {
 /* PF controller (Brogan video) - finish PIDF
 Two different target velocities to toggle between - 900rpm, 1500rpm
 
-check how to access actualvelocity variables after updated in setRPM
+- check how to access actualvelocity variables after updated in setRPM#
+- atSpeed() func
 
  */
