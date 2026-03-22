65893e826cd2d43cfe28d2a5adc665d3b6c5f1c6
Kishika <kishi.j21@gmail.com>
2026-03-22 15:02:36 +0000

    skeleton code + loop logic

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Intake.java
@@ -5,18 +5,29 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
+/*
+GAMEPAD:
+d pad up held - intake reversed
+ */
+
 public class Intake {
 
     private DcMotorEx intakeMotor;
 
+    public static double intakePower = 0.6; // change to default intake motor speed
     // MAIN METHODS
 
     // constructor method
     public Intake(HardwareMapConfig hw) {
         intakeMotor = hw.intake_motor;
     }
-    public void loop(Gamepad gp) {
 
+    public void loop(Gamepad gp) {
+        intakeMotor.setPower(intakePower);
+
+        if(gp.dpad_up) {
+            intakeMotor.setPower(-intakePower);
+        }
     }
 
     public void updateTelemetry(Telemetry t) {
